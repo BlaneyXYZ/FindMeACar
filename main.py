04048614721ee2bf1c6211913688fb8f574a8026
Mu5tank05 <nathanblaney98@gmail.com>
2018-09-06 10:06:28 +1000

    more cars

--- main.py
+++ main.py
@@ -10,7 +10,6 @@ def car_scrape(search_string):
     car_price = soup.find('div', attrs = {'class': 'price-value'})
     car_power_kw = soup.find('div', attrs = {'id': 'description-power-0'})
     car_power_torque = soup.find('div', attrs = {'id': 'description-torque-0'})
-    car_engine_code = soup.find('div', attrs = {'id': 'description-engine-code-0'})
     car_engine_induction = soup.find('div', attrs = {'id': 'description-induction-0'})
     car_engine_cylinders = soup.find('div', attrs = {'id': 'description-cylinders-0'})
     car_drive_type = soup.find('div', attrs = {'id': 'description-drive-0'})
@@ -20,7 +19,6 @@ def car_scrape(search_string):
                          car_price.text.strip().replace("*", ""),
                          car_power_kw.text.strip(),
                          car_power_torque.text.strip(),
-                         car_engine_code.text.strip(),
                          car_engine_induction.text.strip(),
                          car_engine_cylinders.text.strip(),
                          car_drive_type.text.strip(),
